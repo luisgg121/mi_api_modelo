1e619f0095d2d668fc5d252d6f663ed6e89a13e1
luisgg121 <luisgg12_1@hotmail.com>
2025-08-01 18:51:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Esto permite peticiones desde cualquier origen
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
